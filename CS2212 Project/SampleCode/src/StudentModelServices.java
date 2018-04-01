448d8a40ce37929e6cd4c529666d52e880a7f1ac
dorobekd <37968890+dorobekd@users.noreply.github.com>
2018-03-31 22:44:26 -0400

    Add files via upload

---
+++ CS2212 Project/SampleCode/src/StudentModelServices.java
@@ -0,0 +1,15 @@
+
+public class StudentModelServices {
+
+	public void EnrollStudentInCourse(String name, String surname, String id {
+		
+	}
+	
+	public String PrintStudentRecord(String id, String studentCourse) {
+		String studentId = id;
+		String course = studentCourse;
+		StudentModel<> =  
+		
+	}
+	
+}
